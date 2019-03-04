52be4d5a65d61fee5d3241b301b9f5f2062f9d2f
Siyuan Sun <thusiyuansun@gmail.com>
2019-03-04 13:41:45 +0800

    new file

---
+++ symmetry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+############################################################################
+#  author: Siyuan Sun
+#  e-mail: thusiyuansun@gmail.com
+#  version: 0.0
+#  details: new file
+#
+#  @brief  Visualize the symmetry operation of a certain point in real space
+#  For example, can a combination of several symmetry operations cover the 
+#  whole parts of a symmetry element ? Visualization is always the best test.
+############################################################################
